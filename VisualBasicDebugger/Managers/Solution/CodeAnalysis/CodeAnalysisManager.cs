7bce22447557f649cfb4c95c19d4e70faf8c77b7
AntonVonDelta <25268629+AntonVonDelta@users.noreply.github.com>
2023-03-12 01:18:45 +0200

    Added merkle tree to code analysis

--- VisualBasicDebugger/Managers/Solution/CodeAnalysis/CodeAnalysisManager.cs
+++ VisualBasicDebugger/Managers/Solution/CodeAnalysis/CodeAnalysisManager.cs
@@ -4,22 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VisualBasicDebugger.Managers.Solution.Documents;
+using VisualBasicDebugger.Utils;
 
 namespace VisualBasicDebugger.Managers.Solution.CodeAnalysis {
     public class CodeAnalysisManager {
+        private MerkleTree<VBDocument, SolutionAnalysis> _solutionMerkleTree;
         private List<VBDocument> _documents = new List<VBDocument>();
+
+        public event Action<SolutionAnalysis> FinishedAnalysis;
+
+        public CodeAnalysisManager() {
+            _solutionMerkleTree = new MerkleTree<VBDocument, SolutionAnalysis>(MerkleTransformer, MerkleReducer);
+        }
 
         public void AddDocument(VBDocument document) {
             _documents.Add(document);
+            document.Changed += Document_Changed;
+
+            _solutionMerkleTree.Add(document);
+            OnFinishedAnalysis();
         }
 
+        private void Document_Changed(Document document) {
+            var vbdocument = (VBDocument)document;
 
-        public void Analyze(string input) {
-
+            _solutionMerkleTree.UpdateEntry(vbdocument, vbdocument);
         }
 
-        public void GetScopedVariables(int position) {
+        private SolutionAnalysis MerkleTransformer(VBDocument document) {
+            return null;
+        }
 
+        private SolutionAnalysis MerkleReducer(SolutionAnalysis firstAnalysis, SolutionAnalysis secondAnalysis) {
+            return null;
+        }
+
+        private void OnFinishedAnalysis() {
+            if (FinishedAnalysis != null) FinishedAnalysis(_solutionMerkleTree.Result);
         }
     }
 }
---
+++ VisualBasicDebugger/Managers/Solution/CodeAnalysis/SolutionAnalysis.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualBasicDebugger.Managers.Solution.CodeAnalysis.SemanticModels;
+
+namespace VisualBasicDebugger.Managers.Solution.CodeAnalysis {
+    public class SolutionAnalysis {
+        public List<Function> Functions { get; set; }
+    }
+}
