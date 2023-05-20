20c0921c3604c98552e9d8dd4f0dd3c1da8671eb
AntonVonDelta <25268629+AntonVonDelta@users.noreply.github.com>
2023-05-20 22:22:51 +0300

    Fixed designer: added Lexilla.dll and Scintilla x64 dlls to AppData\Local\Microsoft\VisualStudio\17.0_f0a9c9ca\build\x64

--- VisualBasicDebugger/Forms/Editor/FormEditor.Designer.cs
+++ VisualBasicDebugger/Forms/Editor/FormEditor.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using ScintillaNET;
+using System.Resources;
 using VisualBasicDebugger.Properties;
 
 namespace VisualBasicDebugger.Forms.Editor {
@@ -26,18 +27,18 @@ namespace VisualBasicDebugger.Forms.Editor {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEditor));
             this.tableLayoutPrincipal = new System.Windows.Forms.TableLayoutPanel();
+            this.tabView = new System.Windows.Forms.TabControl();
+            this.tabOutput = new System.Windows.Forms.TabPage();
+            this.tabCollapse = new System.Windows.Forms.TabPage();
             this.tabDocuments = new System.Windows.Forms.TabControl();
             this.tableLayoutSideRight = new System.Windows.Forms.TableLayoutPanel();
             this.btnGenerateTraceCode = new System.Windows.Forms.Button();
-            this.tabCollapse = new System.Windows.Forms.TabPage();
-            this.tabOutput = new System.Windows.Forms.TabPage();
-            this.tabView = new System.Windows.Forms.TabControl();
             this.treeSolutionView = new System.Windows.Forms.TreeView();
+            this.mainTextEditor = new ScintillaNET.Scintilla();
             this.tableLayoutPrincipal.SuspendLayout();
-            this.tableLayoutSideRight.SuspendLayout();
             this.tabView.SuspendLayout();
+            this.tableLayoutSideRight.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPrincipal
@@ -48,6 +49,7 @@ namespace VisualBasicDebugger.Forms.Editor {
             this.tableLayoutPrincipal.Controls.Add(this.tabView, 0, 2);
             this.tableLayoutPrincipal.Controls.Add(this.tabDocuments, 0, 0);
             this.tableLayoutPrincipal.Controls.Add(this.tableLayoutSideRight, 1, 0);
+            this.tableLayoutPrincipal.Controls.Add(this.mainTextEditor, 0, 1);
             this.tableLayoutPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPrincipal.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPrincipal.Name = "tableLayoutPrincipal";
@@ -57,6 +59,40 @@ namespace VisualBasicDebugger.Forms.Editor {
             this.tableLayoutPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPrincipal.Size = new System.Drawing.Size(1253, 716);
             this.tableLayoutPrincipal.TabIndex = 8;
+            // 
+            // tabView
+            // 
+            this.tabView.Controls.Add(this.tabOutput);
+            this.tabView.Controls.Add(this.tabCollapse);
+            this.tabView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabView.HotTrack = true;
+            this.tabView.Location = new System.Drawing.Point(3, 496);
+            this.tabView.Name = "tabView";
+            this.tabView.SelectedIndex = 0;
+            this.tabView.Size = new System.Drawing.Size(1097, 217);
+            this.tabView.TabIndex = 14;
+            this.tabView.SelectedIndexChanged += new System.EventHandler(this.tabView_SelectedIndexChanged);
+            // 
+            // tabOutput
+            // 
+            this.tabOutput.Location = new System.Drawing.Point(4, 22);
+            this.tabOutput.Name = "tabOutput";
+            this.tabOutput.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOutput.Size = new System.Drawing.Size(1089, 191);
+            this.tabOutput.TabIndex = 0;
+            this.tabOutput.Text = "Output";
+            this.tabOutput.UseVisualStyleBackColor = true;
+            // 
+            // tabCollapse
+            // 
+            this.tabCollapse.Location = new System.Drawing.Point(4, 22);
+            this.tabCollapse.Name = "tabCollapse";
+            this.tabCollapse.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCollapse.Size = new System.Drawing.Size(1089, 191);
+            this.tabCollapse.TabIndex = 1;
+            this.tabCollapse.Tag = "Collapse";
+            this.tabCollapse.Text = "↓";
+            this.tabCollapse.UseVisualStyleBackColor = true;
             // 
             // tabDocuments
             // 
@@ -96,40 +132,6 @@ namespace VisualBasicDebugger.Forms.Editor {
             this.btnGenerateTraceCode.UseVisualStyleBackColor = true;
             this.btnGenerateTraceCode.Click += new System.EventHandler(this.btnGenerateTraceCode_Click);
             // 
-            // tabCollapse
-            // 
-            this.tabCollapse.Location = new System.Drawing.Point(4, 22);
-            this.tabCollapse.Name = "tabCollapse";
-            this.tabCollapse.Padding = new System.Windows.Forms.Padding(3);
-            this.tabCollapse.Size = new System.Drawing.Size(1089, 191);
-            this.tabCollapse.TabIndex = 1;
-            this.tabCollapse.Tag = "Collapse";
-            this.tabCollapse.Text = "↓";
-            this.tabCollapse.UseVisualStyleBackColor = true;
-            // 
-            // tabOutput
-            // 
-            this.tabOutput.Location = new System.Drawing.Point(4, 22);
-            this.tabOutput.Name = "tabOutput";
-            this.tabOutput.Padding = new System.Windows.Forms.Padding(3);
-            this.tabOutput.Size = new System.Drawing.Size(1089, 191);
-            this.tabOutput.TabIndex = 0;
-            this.tabOutput.Text = "Output";
-            this.tabOutput.UseVisualStyleBackColor = true;
-            // 
-            // tabView
-            // 
-            this.tabView.Controls.Add(this.tabOutput);
-            this.tabView.Controls.Add(this.tabCollapse);
-            this.tabView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabView.HotTrack = true;
-            this.tabView.Location = new System.Drawing.Point(3, 496);
-            this.tabView.Name = "tabView";
-            this.tabView.SelectedIndex = 0;
-            this.tabView.Size = new System.Drawing.Size(1097, 217);
-            this.tabView.TabIndex = 14;
-            this.tabView.SelectedIndexChanged += new System.EventHandler(this.tabView_SelectedIndexChanged);
-            // 
             // treeSolutionView
             // 
             this.treeSolutionView.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -137,6 +139,24 @@ namespace VisualBasicDebugger.Forms.Editor {
             this.treeSolutionView.Name = "treeSolutionView";
             this.treeSolutionView.Size = new System.Drawing.Size(138, 502);
             this.treeSolutionView.TabIndex = 14;
+            // 
+            // mainTextEditor
+            // 
+            this.mainTextEditor.AutoCMaxHeight = 9;
+            this.mainTextEditor.BiDirectionality = ScintillaNET.BiDirectionalDisplayType.Disabled;
+            this.mainTextEditor.CaretLineBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.mainTextEditor.CaretLineFrame = 1;
+            this.mainTextEditor.CaretLineVisible = true;
+            this.mainTextEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTextEditor.LexerName = null;
+            this.mainTextEditor.Location = new System.Drawing.Point(3, 29);
+            this.mainTextEditor.Name = "mainTextEditor";
+            this.mainTextEditor.ScrollWidth = 345;
+            this.mainTextEditor.Size = new System.Drawing.Size(1097, 461);
+            this.mainTextEditor.TabIndents = true;
+            this.mainTextEditor.TabIndex = 12;
+            this.mainTextEditor.UseRightToLeftReadingLayout = false;
+            this.mainTextEditor.WrapMode = ScintillaNET.WrapMode.None;
             // 
             // FormEditor
             // 
@@ -149,8 +169,8 @@ namespace VisualBasicDebugger.Forms.Editor {
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormEditor_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPrincipal.ResumeLayout(false);
-            this.tableLayoutSideRight.ResumeLayout(false);
             this.tabView.ResumeLayout(false);
+            this.tableLayoutSideRight.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -165,6 +185,7 @@ namespace VisualBasicDebugger.Forms.Editor {
         private System.Windows.Forms.TabPage tabOutput;
         private System.Windows.Forms.TabPage tabCollapse;
         private System.Windows.Forms.TreeView treeSolutionView;
+        private Scintilla mainTextEditor;
     }
 }
 
